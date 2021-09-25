bc7d7bf377a07944ff4769a0de8fb4dc0dd258f7
Alexander Blaus <devsasokl@gmail.com>
2021-09-25 14:30:50 +0300

    add card logic added

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 //=============== Initialization ===============//
-
-// Buttons
+const cards = document.querySelector('.cards');
+// Buttons =====
 // popup profile buttons
 const buttonOpenProfilePopup = document.querySelector('.profile__edit-button');
 const buttonCloseProfilePopup = document.querySelector('.popup__close-button_type_profile');
@@ -8,7 +8,7 @@ const buttonCloseProfilePopup = document.querySelector('.popup__close-button_typ
 const buttonOpenAddCardPopup = document.querySelector('.profile__add-button');
 const buttonCloseAddCardPopup = document.querySelector('.popup__close-button_type_add-card');
 
-// Inputs
+// Inputs =====
 // popup profile inputs
 const inputProfileNamePopup = document.querySelector('#profile-name-input');
 const inputProfileAboutPopup = document.querySelector('#profile-about-input');
@@ -16,17 +16,21 @@ const inputProfileAboutPopup = document.querySelector('#profile-about-input');
 const inputCardTitlePopup = document.querySelector('#card-title-input');
 const inputCardImageLinkPopup = document.querySelector('#card-image-link-input');
 
-// Text elements
+// Text elements =====
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
 
-// Popups
+// Popups =====
 // popup profile
 const popupProfile = document.querySelector('.popup_type_profile');
 const popupProfileForm = document.querySelector('.popup__form_type_profile');
 // popup profile
 const popupAddCard = document.querySelector('.popup_type_add-card');
 const popupAddCardForm = document.querySelector('.popup__form_type_add-card');
+
+// Templates =====
+// template card
+const templateCard = document.querySelector('#card-template').content;
 
 // Cards
 const initialCards = [
@@ -56,6 +60,9 @@ const initialCards = [
     }
 ];
 
+initialCards.forEach((card) => {
+    addCard(card.name, card.link);
+});
 //=============== Functions ===============//
 
 function popupClose(popup) {
@@ -66,6 +73,13 @@ function popupOpen(popup) {
     popup.classList.toggle('popup_open');
 }
 
+function addCard(name, link){
+    const newCard = templateCard.querySelector('.card').cloneNode(true);
+    newCard.querySelector('.card__image').alt = name;
+    newCard.querySelector('.card__image').src = link;
+    newCard.querySelector('.card__title').textContent = name;
+    cards.append(newCard);
+}
 /*function cardLikeToggle(button) {
     button.classList.toggle('card__like-button_active');
 }*/
@@ -94,6 +108,7 @@ popupProfileForm.addEventListener('submit', (e) => {
 
 popupAddCardForm.addEventListener('submit',(e) => {
     e.preventDefault();
+    addCard(inputCardTitlePopup.value, inputCardImageLinkPopup.value);
     popupClose(popupAddCard);
 })
 
